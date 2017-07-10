1af36326ec9e7bd3e93b52c212ef7de28a120658
Akram Khan <alorweb@gmail.com>
2017-07-10 05:34:53 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(b)
+  row =[" #{b[0]} | #{b[1]} | #{b[2]} ", " #{b[3]} | #{b[4]} | #{b[5]} ", " #{b[6]} | #{b[7]} | #{b[8]} "]
+  puts row[0]
+  puts "-----------"
+  puts row[1]
+  puts "-----------"
+  puts row[2]
+end
